6dc92fe20ace18d788169c55dd7d0d4ec011e8da
Joseph Martin <se.josephmartin@gmail.com>
2024-11-27 21:15:31 +0800

    add additional routes

--- api/index.js
+++ api/index.js
@@ -12,6 +12,14 @@ app.use(
 	})
 );
 
+app.get('/', (req, res) => {
+	res.send('a simple express server for fetching dev.to articles');
+});
+
+app.use((req, res, next) => {
+	res.status(404).send('404 Not Found');
+});
+
 app.get('/api/dev-blogs', async (req, res) => {
 	const username = 'jfmartinz';
 	const devToApiUrl = `https://dev.to/api/articles?username=${username}`;
